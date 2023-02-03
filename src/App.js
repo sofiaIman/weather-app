ef3ae9032b70441f1bfcda4ead87f5c396f5705c
sofia <sofia.iman@strathmore.edu>
2023-02-03 21:57:16 +0300

    UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 
 import './App.css';
 import Inputs from './components/Inputs';
+import TemperatureAndDetails from './components/TemperatureAndDetails';
+import TimeAndLocation from './components/TimeAndLocation';
 import TopButton from './components/TopButton';
+import Forecast from './components/Forecast';
 
 function App() {
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
     <TopButton />
     <Inputs />
+
+    <TimeAndLocation />
+    <TemperatureAndDetails />
+
+    <Forecast title="hourly forecast"/>
+    <Forecast title="daily forecast"/>
     </div>
   );
 }
